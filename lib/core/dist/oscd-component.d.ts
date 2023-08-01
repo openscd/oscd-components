493351440b8e5c2b69d137615cde4c616a453e92
Pascal Wilbrink <pascal.wilbrink@alliander.com>
2023-08-01 14:50:54 +0200

    fix: Added newline for release-please trigger

--- lib/core/dist/oscd-component.d.ts
+++ lib/core/dist/oscd-component.d.ts
@@ -1,5 +1,3 @@
 import { LitElement } from 'lit-element';
-declare const OscdComponent_base: typeof LitElement & import("@open-wc/dedupe-mixin").Constructor<import("@open-wc/scoped-elements/types/src/types").ScopedElementsHost>;
-export declare abstract class OscdComponent extends OscdComponent_base {
+export declare abstract class OscdComponent extends LitElement {
 }
-export {};
--- lib/core/src/oscd-component.ts
+++ lib/core/src/oscd-component.ts
@@ -1,4 +1,3 @@
 import { LitElement } from 'lit-element';
-import { ScopedElementsMixin } from '@open-wc/scoped-elements';
 
-export abstract class OscdComponent extends ScopedElementsMixin(LitElement) {}
+export abstract class OscdComponent extends LitElement {}

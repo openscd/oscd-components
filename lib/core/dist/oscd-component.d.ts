dfb2c0030e96d0a5860a4d35f526a17517a6c7d9
Pascal Wilbrink <pascal.wilbrink@alliander.com>
2023-08-01 14:40:24 +0200

    Moved scoped-elements dependency to dependencies instead of dev

---
+++ lib/core/dist/oscd-component.d.ts
@@ -0,0 +1,5 @@
+import { LitElement } from 'lit-element';
+declare const OscdComponent_base: typeof LitElement & import("@open-wc/dedupe-mixin").Constructor<import("@open-wc/scoped-elements/types/src/types").ScopedElementsHost>;
+export declare abstract class OscdComponent extends OscdComponent_base {
+}
+export {};

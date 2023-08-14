538e1d521caa99889721b07e4cf7e5899759cdd6
Pascal Wilbrink <pascal.wilbrink@alliander.com>
2023-08-14 12:45:03 +0200

    Feat: Added Design tokens

---
+++ themes/built/oscd/variables.js
@@ -0,0 +1,30 @@
+/**
+ * Do not edit directly
+ * Generated on Mon, 07 Aug 2023 08:14:41 GMT
+ */
+
+export const ColorOscdWhite = "#ffffff";
+export const ColorOscdBlack = "#000000";
+export const ColorOscdSysLightPrimary = "#389add";
+export const ColorOscdSysLightOnPrimary = "#ffffff";
+export const ColorOscdSysLightSecondary = "#ae7ce9";
+export const ColorOscdSysLightSuccess = "#66bf3b";
+export const ColorOscdSysLightOnSuccess = "#ffffff";
+export const ColorOscdSysLightInfo = "#3b72bf";
+export const ColorOscdSysLightOnInfo = "#ffffff";
+export const ColorOscdSysLightWarning = "#ea9e08";
+export const ColorOscdSysLightOnWarning = "#ffffff";
+export const ColorOscdSysLightDanger = "#bf3b3b";
+export const ColorOscdSysLightOnDanger = "#ffffff";
+export const ColorOscdRefPrimaryPrimary100 = "#b2dbf8";
+export const ColorOscdRefPrimaryPrimary200 = "#72bcf0";
+export const ColorOscdRefPrimaryPrimary300 = "#389add";
+export const ColorOscdRefPrimaryPrimary400 = "#1779bd";
+export const ColorOscdRefPrimaryPrimary500 = "#0a5b93";
+export const ColorOscdRefPrimaryPrimary600 = "#053e66";
+export const ColorOscdRefSecondarySecondary100 = "#e9caff";
+export const ColorOscdRefSecondarySecondary200 = "#d0a0ff";
+export const ColorOscdRefSecondarySecondary300 = "#ae7ce9";
+export const ColorOscdRefSecondarySecondary400 = "#8c61be";
+export const ColorOscdRefSecondarySecondary500 = "#6a4792";
+export const ColorOscdRefSecondarySecondary600 = "#483164";
